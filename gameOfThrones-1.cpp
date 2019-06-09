4ce7c09a8ee23482d16b7f84a23366afe512dc45
YogaVicky <yogasrivarshan@gmail.com>
2019-06-09 12:09:49 +0530

    update

---
+++ gameOfThrones-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+void checksymmetry(){
+	cout<<"Yes";
+	cout<<"No";
+}
+int main(){
+	string a;
+	getline(cin,a);
+	checksymmetry(a);
+	return 0;
+}
