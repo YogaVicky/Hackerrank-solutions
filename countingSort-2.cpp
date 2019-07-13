435df2ddb99a1ef5a70dd21bdc1e68067f5c666d
YogaVicky <yogasrivarshan@gmail.com>
2019-07-13 19:02:18 +0530

    update

--- countingSort-2.cpp
+++ countingSort-2.cpp
@@ -3,14 +3,15 @@
 #include<math.h>
 using namespace std;
 int main(){
-	long int n , i , a[1000000] , b[100] = {0};
+	long int n , i , a[1000000] , b[100] = {0} , j;
 	cin>>n;
 	for(i=0;i<n;i++){
 		cin>>a[i];
 		b[a[i]]++;
 	}
 	for(i=0;i<100;i++){
-		cout<<b[i]<<" ";
+		for(j=0;j<b[i];j++)
+			cout<<i<<" ";
 	}
 	return 0;
 }
