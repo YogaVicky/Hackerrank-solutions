11cbb5a83acda2146cbc868d5aedca6a2edf2557
YogaVicky <yogasrivarshan@gmail.com>
2019-09-01 19:16:30 +0530

    update

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int 
+	int a[100000][100000] , lastAnswer = 0 , n , q;
+	cin>>n>>q;
+	for(i=0;i<q;i++){
+		cin>>t>>x>>y;
+		if(t==1){
+			i1 = x
+		}
+		else{
+
+		}
+	}
 }
