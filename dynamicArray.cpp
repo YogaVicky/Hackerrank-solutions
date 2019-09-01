a5610f1244bb0418e5583a6a5ca639471fe7ef7f
YogaVicky <yogasrivarshan@gmail.com>
2019-09-01 23:56:02 +0530

    update

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a[100000][100000] , lastAnswer = 0 , n , q;
+	int a[100000][5] , q , n , b[100000] = {0} , lastanswer = 0;
 	cin>>n>>q;
-	for(i=0;i<q;i++){
-		cin>>t>>x>>y;
-		if(t==1){
-			i1 = x
+	while(q--){
+		cin>>s>>x>>y;
+		if(s==1){
+			a[(x^lastanswer)%n][b[(x^lastanswer)%n]] = y;
+			b[(x^lastanswer)%n]++;
 		}
 		else{
-
+			lastanswer = y%b[(x^lastanswer)%n];
+			cout<<lastanswer<<endl;
 		}
 	}
+	return 0;
 }
